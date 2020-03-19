fbeb32aa7ca0713464abf439e3b88a304b8d8d80
JereChen <jerechen11@gmail.com>
2020-03-19 23:10:48 +0800

    add Home bottom bar item custom view

--- app/src/main/java/com/jere/wanandroid_learning_kotlin/view/MainActivity.kt
+++ app/src/main/java/com/jere/wanandroid_learning_kotlin/view/MainActivity.kt
@@ -3,7 +3,6 @@ package com.jere.wanandroid_learning_kotlin.view
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -16,26 +15,40 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import com.jere.wanandroid_learning_kotlin.R
+import com.jere.wanandroid_learning_kotlin.utils.HomeBottomBarItemCustomView
 import com.jere.wanandroid_learning_kotlin.view.completeproject.CompleteProjectFragment
 import com.jere.wanandroid_learning_kotlin.view.home.HomeFragment
 import com.jere.wanandroid_learning_kotlin.view.knowledgesystem.KnowledgeSystemFragment
 import com.jere.wanandroid_learning_kotlin.view.wechat.WeChatFragment
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.home_btn -> addFragment(HomeFragment(), true, "homeBtn")
-            R.id.complete_project_btn -> addFragment(
-                CompleteProjectFragment(),
-                false,
-                "completeProject"
-            )
-            R.id.we_chart_btn -> addFragment(WeChatFragment(), false, "weChart")
-            R.id.knowledge_system_btn -> addFragment(
-                KnowledgeSystemFragment(),
-                false,
-                "knowledgeSystemBtn"
-            )
+            R.id.home_bottom_bar_home_item -> {
+                addFragment(HomeFragment(), true, "homeBtn")
+                toolbar.title = getString(R.string.menu_home)
+            }
+            R.id.home_bottom_bar_complete_project_item -> {
+                addFragment(
+                    CompleteProjectFragment(),
+                    false,
+                    "completeProject"
+                )
+                toolbar.title = getString(R.string.menu_complete_project)
+            }
+            R.id.home_bottom_bar_we_chart_item -> {
+                addFragment(WeChatFragment(), false, "weChart")
+                toolbar.title = getString(R.string.menu_we_chart)
+            }
+            R.id.home_bottom_bar_knowledge_system_item -> {
+                addFragment(
+                    KnowledgeSystemFragment(),
+                    false,
+                    "knowledgeSystemBtn"
+                )
+                toolbar.title = getString(R.string.menu_knowledge_system)
+            }
         }
     }
 
@@ -73,14 +86,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        val homeBtn: Button = findViewById(R.id.home_btn)
-        val completeProjectBtn: Button = findViewById(R.id.complete_project_btn)
-        val weChartBtn: Button = findViewById(R.id.we_chart_btn)
-        val knowledgeSystemBtn: Button = findViewById(R.id.knowledge_system_btn)
-        homeBtn.setOnClickListener(this)
-        completeProjectBtn.setOnClickListener(this)
-        weChartBtn.setOnClickListener(this)
-        knowledgeSystemBtn.setOnClickListener(this)
+        val homeBarItem: HomeBottomBarItemCustomView = findViewById(R.id.home_bottom_bar_home_item)
+        val completeProjectItem: HomeBottomBarItemCustomView =
+            findViewById(R.id.home_bottom_bar_complete_project_item)
+        val weChartItem: HomeBottomBarItemCustomView =
+            findViewById(R.id.home_bottom_bar_we_chart_item)
+        val knowledgeSystemItem: HomeBottomBarItemCustomView =
+            findViewById(R.id.home_bottom_bar_knowledge_system_item)
+        homeBarItem.setOnClickListener(this)
+        completeProjectItem.setOnClickListener(this)
+        weChartItem.setOnClickListener(this)
+        knowledgeSystemItem.setOnClickListener(this)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
