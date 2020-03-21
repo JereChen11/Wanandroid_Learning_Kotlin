3d5c3a0b2b4c55eec00c74d74f152299e545e17b
JereChen <jerechen11@gmail.com>
2020-03-21 21:37:35 +0800

    implement home banner infinate loop and  auto loop feature

--- app/src/main/java/com/jere/wanandroid_learning_kotlin/view/home/HomeFragment.kt
+++ app/src/main/java/com/jere/wanandroid_learning_kotlin/view/home/HomeFragment.kt
@@ -2,6 +2,8 @@ package com.jere.wanandroid_learning_kotlin.view.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,10 +18,13 @@ import com.bumptech.glide.Glide
 import com.jere.wanandroid_learning_kotlin.R
 import com.jere.wanandroid_learning_kotlin.model.homebeanfiles.HomeArticleListBean
 import com.jere.wanandroid_learning_kotlin.model.homebeanfiles.HomeBannerListBean
-import com.jere.wanandroid_learning_kotlin.view.ArticleDetailWebViewActivity
 import com.jere.wanandroid_learning_kotlin.utils.RecyclerItemClickListener
+import com.jere.wanandroid_learning_kotlin.view.ArticleDetailWebViewActivity
 import com.jere.wanandroid_learning_kotlin.viewmodel.home.HomeViewModel
 import java.lang.ref.WeakReference
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
 
 
 class HomeFragment : Fragment() {
@@ -27,6 +32,10 @@ class HomeFragment : Fragment() {
     private lateinit var homeViewModel: HomeViewModel
     private var mHomeArticleListData: ArrayList<HomeArticleListBean.DataBean.DatasBean> =
         ArrayList()
+    private var mHomeBannerListData: ArrayList<HomeBannerListBean.DataBean> = ArrayList()
+    private lateinit var mHomeBannerVp: ViewPager2
+    private lateinit var mHomeBannerHandler: HomeBannerHandler
+    private lateinit var mHomeBannerExecutorService: ScheduledExecutorService
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,46 +50,74 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val homeBannerVp2: ViewPager2 = view.findViewById(R.id.home_banner_vp2)
-        val homeArticleListRecyclerView: RecyclerView =
-            view.findViewById(R.id.home_article_list_recycle_view)
+        mHomeBannerHandler = HomeBannerHandler(this)
 
+        initHomeBannerViewPager(view)
+        initHomeArticleListRecyclerView(view)
 
-        homeViewModel.homeBannerListLd.observe(viewLifecycleOwner, Observer {
-            homeBannerVp2.adapter = ViewPagerAdapter(this, it)
-        })
+        startAutoLoopBanner()
+    }
 
-        homeViewModel.homeArticleListLd.observe(viewLifecycleOwner, Observer {
-            mHomeArticleListData = it
-            homeArticleListRecyclerView.adapter = ArticleListAdapter(it)
-
-        })
-
-        homeViewModel.setHomeBannerList()
-        homeViewModel.setHomeArticleList(1)
-
-
+    private fun initHomeBannerViewPager(view: View) {
         val firstView: View = view.findViewById(R.id.first_indicate_view)
         val secondView: View = view.findViewById(R.id.second_indicate_view)
         val thirdView: View = view.findViewById(R.id.third_indicate_view)
         val fourthView: View = view.findViewById(R.id.fourth_indicate_view)
         val indicateViews = arrayOf(firstView, secondView, thirdView, fourthView)
 
-        homeBannerVp2.currentItem = 0
-        homeBannerVp2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+        mHomeBannerVp = view.findViewById(R.id.home_banner_vp2)
+        mHomeBannerVp.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-                for (i in 0..3) {
-                    if (position == i) {
-                        indicateViews[i]
-                            .setBackgroundResource(R.drawable.banner_navigation_point_highlight_shape)
+
+                var bannerPosition = position
+                //change view page position, if position on banner head and end.
+                if (bannerPosition == 0) {
+                    bannerPosition = 4
+                } else if (bannerPosition == 5) {
+                    bannerPosition = 1
+                }
+
+                for (i in 1..4) {
+                    if (bannerPosition == i) {
+                        indicateViews[i - 1].setBackgroundResource(R.drawable.banner_navigation_point_highlight_shape)
                     } else {
-                        indicateViews[i]
-                            .setBackgroundResource(R.drawable.banner_navigation_point_gray_shape)
+                        indicateViews[i - 1].setBackgroundResource(R.drawable.banner_navigation_point_gray_shape)
                     }
+                }
+
+                //if bannerPosition != position, meaning you are in the banner head or end,
+                // you need to change viewPager position to implement banner loop feature
+                if (bannerPosition != position) {
+                    mHomeBannerVp.currentItem = bannerPosition
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                super.onPageScrollStateChanged(state)
+                if (state == ViewPager2.SCROLL_STATE_DRAGGING && !mHomeBannerExecutorService.isShutdown) {
+                    mHomeBannerExecutorService.shutdown()
                 }
             }
         })
+
+        homeViewModel.homeBannerListLd.observe(viewLifecycleOwner, Observer {
+            mHomeBannerListData = ArrayList()
+            //add two fake data on list head and end to implement banner loop feature.
+            val emptyHomeBannerData: HomeBannerListBean.DataBean = HomeBannerListBean.DataBean()
+            mHomeBannerListData.add(emptyHomeBannerData)
+            mHomeBannerListData.addAll(it)
+            mHomeBannerListData.add(emptyHomeBannerData)
+            mHomeBannerVp.adapter = ViewPagerAdapter(this, mHomeBannerListData)
+            mHomeBannerVp.currentItem = 1
+        })
+
+        homeViewModel.setHomeBannerList()
+    }
+
+    private fun initHomeArticleListRecyclerView(view: View) {
+        val homeArticleListRecyclerView: RecyclerView =
+            view.findViewById(R.id.home_article_list_recycle_view)
 
         homeArticleListRecyclerView.addOnItemTouchListener(
             RecyclerItemClickListener(context,
@@ -102,6 +139,22 @@ class HomeFragment : Fragment() {
                 })
         )
 
+        homeViewModel.homeArticleListLd.observe(viewLifecycleOwner, Observer {
+            mHomeArticleListData = it
+            homeArticleListRecyclerView.adapter = ArticleListAdapter(it)
+
+        })
+
+        homeViewModel.setHomeArticleList(1)
+    }
+
+    private fun startAutoLoopBanner() {
+        mHomeBannerExecutorService = Executors.newScheduledThreadPool(1)
+        mHomeBannerExecutorService.scheduleAtFixedRate({
+            val msg = Message()
+            msg.what = 1
+            mHomeBannerHandler.sendMessage(msg)
+        }, 2, 3, TimeUnit.SECONDS)
     }
 
     class ViewPagerAdapter(
@@ -170,5 +223,30 @@ class HomeFragment : Fragment() {
         }
     }
 
+    class HomeBannerHandler(homeFragment: HomeFragment) : Handler() {
+        private val weakReference: WeakReference<HomeFragment> = WeakReference(homeFragment)
+
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            if (msg.what == 1) {
+                val homeFragment: HomeFragment? = weakReference.get()
+                if (homeFragment != null) {
+                    val vpCurrentPosition: Int = homeFragment.mHomeBannerVp.currentItem
+                    if (vpCurrentPosition == 4) {
+                        homeFragment.mHomeBannerVp.currentItem = 1
+                    } else {
+                        homeFragment.mHomeBannerVp.currentItem = vpCurrentPosition + 1
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (!mHomeBannerExecutorService.isShutdown) {
+            mHomeBannerExecutorService.shutdown()
+        }
+    }
 
 }
