6950679e78aa5ffe11999b4f801b04b0a0df4136
JereChen <jerechen11@gmail.com>
2020-08-24 16:21:38 +0800

    change complete project ui

--- app/src/main/java/com/jere/wanandroid_learning_kotlin/view/completeproject/CompleteProjectFragment.kt
+++ app/src/main/java/com/jere/wanandroid_learning_kotlin/view/completeproject/CompleteProjectFragment.kt
@@ -1,21 +1,18 @@
 package com.jere.wanandroid_learning_kotlin.view.completeproject
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import com.jere.wanandroid_learning_kotlin.R
 import com.jere.wanandroid_learning_kotlin.model.completeprojectbeanfiles.ProjectTreeItem
-import com.jere.wanandroid_learning_kotlin.utils.RecyclerItemClickListener
 import com.jere.wanandroid_learning_kotlin.viewmodel.completeproject.CompleteProjectViewModel
+import kotlinx.android.synthetic.main.fragment_complete_project.*
 
 class CompleteProjectFragment : Fragment() {
 
@@ -34,63 +31,103 @@ class CompleteProjectFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val completeProjectRv: RecyclerView = view.findViewById(R.id.complete_project_rv)
-        val layoutManager: RecyclerView.LayoutManager = GridLayoutManager(context, 2)
-        completeProjectRv.layoutManager = layoutManager
-        completeProjectRv.addOnItemTouchListener(RecyclerItemClickListener(context,
-            completeProjectRv, object : RecyclerItemClickListener.OnItemClickListener {
-                override fun onItemClick(view: View?, position: Int) {
-                    Toast.makeText(context, "position = $position", Toast.LENGTH_SHORT).show()
-                    val cid = mProjectTreeItems[position].id
-                    val intent = Intent(context, ProjectItemListActivity::class.java)
-                    intent.putExtra(ProjectItemListActivity.PROJECT_TREE_ITEM_ID_KEY, cid)
-                    startActivity(intent)
-                }
-
-                override fun onLongItemClick(view: View?, position: Int) {
-                    Toast.makeText(
-                        context,
-                        "long Item click position = $position",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-        ))
+//        val completeProjectRv: RecyclerView = view.findViewById(R.id.complete_project_rv)
+//        val layoutManager: RecyclerView.LayoutManager = GridLayoutManager(context, 2)
+//        completeProjectRv.layoutManager = layoutManager
+//        completeProjectRv.addOnItemTouchListener(RecyclerItemClickListener(context,
+//            completeProjectRv, object : RecyclerItemClickListener.OnItemClickListener {
+//                override fun onItemClick(view: View?, position: Int) {
+//
+//                    Toast.makeText(context, "position = $position", Toast.LENGTH_SHORT).show()
+//                    val cid = mProjectTreeItems[position].id
+//                    val intent = Intent(context, ProjectItemListActivity::class.java)
+//                    intent.putExtra(ProjectItemListActivity.PROJECT_TREE_ITEM_ID_KEY, cid)
+//                    startActivity(intent)
+//                }
+//
+//                override fun onLongItemClick(view: View?, position: Int) {
+//                    Toast.makeText(
+//                        context,
+//                        "long Item click position = $position",
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//                }
+//            }
+//        ))
 
         completeProjectVm.projectTreeItemsLd.observe(viewLifecycleOwner, Observer {
             mProjectTreeItems.clear()
             mProjectTreeItems.addAll(it)
-            completeProjectRv.adapter = MyAdapter(mProjectTreeItems)
+
+            initTabLayoutAndVp2()
+
+//            completeProjectRv.adapter = MyAdapter(mProjectTreeItems)
         })
         completeProjectVm.setProjectTreeItems()
+
+
+//        completeProjectTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+//            override fun onTabReselected(tab: TabLayout.Tab?) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab?) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun onTabSelected(tab: TabLayout.Tab?) {
+//                TODO("Not yet implemented")
+//            }
+//
+//        })
     }
 
-    class MyAdapter(projectItems: ArrayList<ProjectTreeItem>) :
-        RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
-        private var projectItems: ArrayList<ProjectTreeItem> = ArrayList()
+    private fun initTabLayoutAndVp2() {
+        completeProjectVp2.adapter = CompleteProjectVp2Adapter(this)
+        //bind TabLayout to ViewPager2
+        TabLayoutMediator(completeProjectTabLayout, completeProjectVp2) { tab, position ->
+            tab.text = mProjectTreeItems[position].name
+        }.attach()
+    }
 
-        init {
-            this.projectItems = projectItems
-        }
+//    class MyAdapter(projectItems: ArrayList<ProjectTreeItem>) :
+//        RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+//        private var projectItems: ArrayList<ProjectTreeItem> = ArrayList()
+//
+//        init {
+//            this.projectItems = projectItems
+//        }
+//
+//        class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//            val contentTv: TextView = itemView.findViewById(R.id.complete_project_content_tv)
+//        }
+//
+//        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+//            val view: View =
+//                LayoutInflater.from(parent.context)
+//                    .inflate(R.layout.recycler_item_view_complete_project, parent, false)
+//            return MyViewHolder(view)
+//        }
+//
+//        override fun getItemCount(): Int {
+//            return projectItems.size
+//        }
+//
+//        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+//            val data: ProjectTreeItem = projectItems[position]
+//            holder.contentTv.text = data.name
+//        }
+//    }
 
-        class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            val contentTv: TextView = itemView.findViewById(R.id.complete_project_content_tv)
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-            val view: View =
-                LayoutInflater.from(parent.context)
-                    .inflate(R.layout.recycler_item_view_complete_project, parent, false)
-            return MyViewHolder(view)
-        }
+    inner class CompleteProjectVp2Adapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
         override fun getItemCount(): Int {
-            return projectItems.size
+            return mProjectTreeItems.size
         }
 
-        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-            val data: ProjectTreeItem = projectItems[position]
-            holder.contentTv.text = data.name
+        override fun createFragment(position: Int): Fragment {
+            return ProjectItemListFragment.newInstance(mProjectTreeItems[position].id)
         }
+
     }
 }
--- app/src/main/java/com/jere/wanandroid_learning_kotlin/view/completeproject/ProjectItemListFragment.kt
+++ app/src/main/java/com/jere/wanandroid_learning_kotlin/view/completeproject/ProjectItemListFragment.kt
@@ -1,69 +1,92 @@
 package com.jere.wanandroid_learning_kotlin.view.completeproject
 
-import android.content.Context
+import android.app.Activity
 import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.jere.wanandroid_learning_kotlin.R
 import com.jere.wanandroid_learning_kotlin.model.articlebeanfile.Article
-import com.jere.wanandroid_learning_kotlin.utils.BaseActivity
 import com.jere.wanandroid_learning_kotlin.utils.RecyclerItemClickListener
 import com.jere.wanandroid_learning_kotlin.view.ArticleDetailWebViewActivity
 import com.jere.wanandroid_learning_kotlin.viewmodel.completeproject.CompleteProjectViewModel
+import kotlinx.android.synthetic.main.activity_project_item_list.*
 import java.lang.ref.WeakReference
 
-class ProjectItemListActivity : BaseActivity() {
+
+class ProjectItemListFragment : Fragment() {
+
+    private var id: Int? = null
+
     companion object {
-        const val PROJECT_TREE_ITEM_ID_KEY = "PROJECT_TREE_ITEM_ID"
+        private const val COMPLETE_PROJECT_ID_KEY = "COMPLETE_PROJECT_ID"
+
+        @JvmStatic
+        fun newInstance(id: Int) =
+            ProjectItemListFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(COMPLETE_PROJECT_ID_KEY, id)
+                }
+            }
     }
 
-    private var mProjectItemListDatas: ArrayList<Article> = ArrayList()
+    private var mProjectItemListData: ArrayList<Article> = ArrayList()
     private var pageNumber = 0
 
-    override fun bindLayout(): Int {
-        return R.layout.activity_project_item_list
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            id = it.getInt(COMPLETE_PROJECT_ID_KEY)
+        }
     }
 
-    override fun initView(view: View?) {
-        val cid = intent.getIntExtra(PROJECT_TREE_ITEM_ID_KEY, 0)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_project_item_list, container, false)
+    }
 
-        val projectItemListRecyclerView: RecyclerView =
-            findViewById(R.id.project_item_list_recycler_view)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        val completeProjectVm: CompleteProjectViewModel = ViewModelProvider(this)[CompleteProjectViewModel::class.java]
-        completeProjectVm.projectItemListLd.observe(this, Observer {
-            mProjectItemListDatas.addAll(it.articles)
-            projectItemListRecyclerView.adapter = MyAdapter(this, mProjectItemListDatas)
+        val completeProjectVm: CompleteProjectViewModel =
+            ViewModelProvider(this)[CompleteProjectViewModel::class.java]
+        completeProjectVm.projectItemListLd.observe(viewLifecycleOwner, Observer {
+            mProjectItemListData.addAll(it.articles)
+            completeProjectRcy.adapter =
+                activity?.let { it1 ->
+                    MyAdapter(it1, mProjectItemListData)
+                }
         })
-        completeProjectVm.setProjectItemList(pageNumber, cid)
+        id?.let { completeProjectVm.setProjectItemList(pageNumber, it) }
 
-        projectItemListRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+        completeProjectRcy.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                 super.onScrollStateChanged(recyclerView, newState)
                 if (!recyclerView.canScrollVertically(1)) {
                     pageNumber++
-                    completeProjectVm.setProjectItemList(pageNumber, cid)
+                    id?.let { completeProjectVm.setProjectItemList(pageNumber, it) }
                 }
             }
         })
 
-        projectItemListRecyclerView.addOnItemTouchListener(
-            RecyclerItemClickListener(this,
-                projectItemListRecyclerView,
+        completeProjectRcy.addOnItemTouchListener(
+            RecyclerItemClickListener(context,
+                completeProjectRcy,
                 object : RecyclerItemClickListener.OnItemClickListener {
                     override fun onItemClick(view: View?, position: Int) {
-                        val link: String? = mProjectItemListDatas[position].link
-                        val intent = Intent(
-                            this@ProjectItemListActivity,
-                            ArticleDetailWebViewActivity::class.java
-                        )
+                        val link: String? = mProjectItemListData[position].link
+                        val intent = Intent(activity, ArticleDetailWebViewActivity::class.java)
                         intent.putExtra(
                             ArticleDetailWebViewActivity.ARTICLE_DETAIL_WEB_LINK_KEY,
                             link
@@ -76,31 +99,29 @@ class ProjectItemListActivity : BaseActivity() {
                     }
                 })
         )
-    }
 
-    override fun doBusiness(mContext: Context?) {
     }
 
     class MyAdapter(
-        projectItemListActivity: ProjectItemListActivity,
+        activity: Activity,
         projectItems: ArrayList<Article>
     ) :
         RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
         private var projectItems: ArrayList<Article> = ArrayList()
-        private var weakReference: WeakReference<ProjectItemListActivity>
+        private var weakReference: WeakReference<Activity>
 
         init {
             this.projectItems = projectItems
-            this.weakReference = WeakReference(projectItemListActivity)
+            this.weakReference = WeakReference(activity)
         }
 
         class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            val envelopIv: ImageView = itemView.findViewById(R.id.project_item_list_envelop_iv)
-            val titleTv: TextView = itemView.findViewById(R.id.project_item_list_title_tv)
+            val envelopIv: ImageView = itemView.findViewById(R.id.envelopIv)
+            val titleTv: TextView = itemView.findViewById(R.id.projectItemListTitleTv)
             val describeContentTv: TextView =
-                itemView.findViewById(R.id.project_item_list_describe_content_tv)
-            val shareDateTv: TextView = itemView.findViewById(R.id.project_item_list_share_date_tv)
-            val authorTv: TextView = itemView.findViewById(R.id.project_item_list_author_tv)
+                itemView.findViewById(R.id.projectItemListDescribeContentTv)
+            val shareDateTv: TextView = itemView.findViewById(R.id.projectItemListShareDateTv)
+            val authorTv: TextView = itemView.findViewById(R.id.projectItemListAuthorTv)
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
